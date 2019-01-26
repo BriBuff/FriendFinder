81521c2433d2320c0ecf0f8181e789696147d4fc
Bri Buffington <bri.kudos@gmail.com>
2019-01-26 14:50:00 -0700

    update

--- server.js
+++ server.js
@@ -3,24 +3,24 @@
 var express = require(express);
 var app = express();
 
-var path = require("path");
+// var path = require("path");
 
 // Port
 
 var PORT = process.env.PORT || 8080;
 
 // Data Parsing
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Routes
 
-require("./app/routing/apiRoutes")(app);
-require("./app/routing/htmlRoutes")(app);
+require(".app/routing/apiRoutes")(app);
+require(".app/routing/htmlRoutes")(app);
+
 
 // Listen
 
 app.listen(PORT, function () {
     console.log("App listening on PORT: http://localhost:" + PORT + "/");
 });
-
