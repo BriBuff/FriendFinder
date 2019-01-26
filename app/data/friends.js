f559c5d9e15bca7b6a4256bb01577c0370dbf0fe
Bri Buffington <bri.kudos@gmail.com>
2019-01-26 14:21:29 -0700

    updated.stats

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,51 +3,87 @@
 var friendsArray = [{
     name: "Alexander Hamilton",
     photo: "https://vignette.wikia.nocookie.net/hamiltonmusical/images/8/85/Alexander.jpg/revision/latest?cb=20160606015522",
-    scores: "[]"
-}, 
-{
-    name: "Aaron Burr",
-    photo: "https://a.wattpad.com/cover/114966174-352-k192074.jpg",
     scores: [
             5,
             5,
             2,
             4,
-            1,
+            2,
             4,
             5,
             5,
             2,
             5,
             5
+        ]
+}, 
+{
+    name: "Aaron Burr",
+    photo: "https://a.wattpad.com/cover/114966174-352-k192074.jpg",
+    scores: [
+            3,
+            4,
+            5,
+            5,
+            4,
+            5,
+            5,
+            4,
+            3,
+            5,
+            4
         ]
 }, 
 {
     name: "Eliza (Schuyler) Hamilton",
     photo: "https://vignette.wikia.nocookie.net/hamiltonmusical/images/0/0b/Elizabeth_Schuyler_Hamilton.jpg/revision/latest?cb=20160805220732",
     scores: [  
-            4,
-            5,
-            5,
-            5,
-            4,
-            5,
-            5,
+            2,
+            1,
+            3,
+            1,
+            1,
+            1,
+            1,
             3,
             5,
             5,
-            5
+            4
         ]
 }, 
 {   
     name: "King George",
     photo: "https://bloximages.newyork1.vip.townnews.com/lancasteronline.com/content/tncms/assets/v3/editorial/4/12/4127f2a8-1130-11e6-8861-dfcaa24c417c/5728a3a5af037.image.jpg?resize=1200%2C1116",
-    scores: "[]"
+    scores: [
+            2,
+            3,
+            2,
+            4,
+            5,
+            2,
+            5,
+            5,
+            3,
+            5,
+            2
+        ]
 }, 
 {
     name: "George Washington",
     photo: "https://vignette.wikia.nocookie.net/hamiltonmusical/images/5/52/George_Washington_1.jpg/revision/latest?cb=20160618005958",
-    scores: "[]"
+    scores: [
+            3,
+            2,
+            3,
+            4,
+            1,
+            2,
+            4,
+            5,
+            4,
+            5,
+            5
+        ]
 }];
 
 // Export Array
