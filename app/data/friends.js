a1ea8101b530f49585fe2286af357a9d4128900d
Bri Buffington <bri.kudos@gmail.com>
2019-01-26 13:53:20 -0700

    friends

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,27 +2,51 @@
 
 var friendsArray = [{
     name: "Alexander Hamilton",
-    photo: "",
+    photo: "https://vignette.wikia.nocookie.net/hamiltonmusical/images/8/85/Alexander.jpg/revision/latest?cb=20160606015522",
     scores: "[]"
 }, 
 {
-    name: "Alexander Hamilton",
-    photo: "",
-    scores: "[]"
+    name: "Aaron Burr",
+    photo: "https://a.wattpad.com/cover/114966174-352-k192074.jpg",
+    scores: [
+            5,
+            5,
+            2,
+            4,
+            1,
+            4,
+            5,
+            5,
+            2,
+            5,
+            5
+        ]
 }, 
 {
-    name: "Alexander Hamilton",
-    photo: "",
-    scores: "[]"
+    name: "Eliza (Schuyler) Hamilton",
+    photo: "https://vignette.wikia.nocookie.net/hamiltonmusical/images/0/0b/Elizabeth_Schuyler_Hamilton.jpg/revision/latest?cb=20160805220732",
+    scores: [  
+            4,
+            5,
+            5,
+            5,
+            4,
+            5,
+            5,
+            3,
+            5,
+            5,
+            5
+        ]
 }, 
 {   
-    name: "Alexander Hamilton",
-    photo: "",
+    name: "King George",
+    photo: "https://bloximages.newyork1.vip.townnews.com/lancasteronline.com/content/tncms/assets/v3/editorial/4/12/4127f2a8-1130-11e6-8861-dfcaa24c417c/5728a3a5af037.image.jpg?resize=1200%2C1116",
     scores: "[]"
 }, 
 {
-    name: "Alexander Hamilton",
-    photo: "",
+    name: "George Washington",
+    photo: "https://vignette.wikia.nocookie.net/hamiltonmusical/images/5/52/George_Washington_1.jpg/revision/latest?cb=20160618005958",
     scores: "[]"
 }];
 
